9c9991111bf9247d71be186bbc8a763320c9a1ac
Sajal Chourasia <sajalchourasia19@gmail.com>
2026-07-22 10:21:29 +0530

    Using insert function on vector

---
+++ vecclear.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> vec={1,2,3,4,5};
+    cout<<"Size before clearing = "<<vec.size()<<endl;
+    cout<<"is empty? = "<<vec.empty()<<endl;
+    vec.clear();
+    cout<<"Size after clearing = "<<vec.size()<<endl;//clears the vector
+    cout<<"is empty? = "<<vec.empty();// 0 = false ; 1 = true
+}
