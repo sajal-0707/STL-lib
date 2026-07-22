1093da584a025cacccec0d16b8223d4db5c85839
Sajal Chourasia <sajalchourasia19@gmail.com>
2026-07-22 10:26:18 +0530

    using erase function using begin

--- vecerase.cpp
+++ vecerase.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-    vector<int> vec={12,34,56,78,90};
+    vector<int> vec= {12,34,56,78,90};
     vec.erase(vec.begin()+2);//begin stays at 0th index so if you want to erase 2nd index do vec.begin()+2
     for(int i=0;i<vec.size();i++)
     {
